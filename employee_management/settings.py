00ef34ca9d090837369cb2c5e75a1dd80078c5e1
tithij02 <tithijangade02@gmail.com>
2025-11-21 14:19:42 +0530

    Fix ALLOWED_HOSTS and settings

--- employee_management/settings.py
+++ employee_management/settings.py
@@ -15,7 +15,12 @@ SECRET_KEY = 'django-insecure-o5^8l=)fh)la8p#azru&7!r@@#&o=lr)c@&e1_16aly2*22(s4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'django-employee-project.onrender.com'
+]
+
 
 
 # Application definition
